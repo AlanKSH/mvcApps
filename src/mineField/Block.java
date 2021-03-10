fe9b4600722eedd0f9ca4cbf103b4847c8c91dcd
JeremyTrieu <jeremytrieu1612@gmail.com>
2021-03-10 15:39:14 -0800

    Add files via upload

---
+++ src/mineField/Block.java
@@ -0,0 +1,31 @@
+import java.io.Serializable;
+public class Block implements Serializable {
+    int xCoor;
+    int yCoor;
+    private Boolean endPoint;
+
+    public Block(int xCoor, int yCoor) {
+        this.xCoor = xCoor;
+        this.yCoor = yCoor;
+        endPoint = false;
+    }
+    public void setX(int xCoor) {
+        this.xCoor = xCoor;
+    }
+    public void setY(int yCoor) {
+        this.yCoor = yCoor;
+    }
+    public int getXCoor() {
+        return xCoor;
+    }
+    public int getYCoor() {
+        return yCoor;
+    }
+    public Boolean getEndPoint() {
+        return endPoint;
+    }
+    public void setEndPoint(Boolean endPoint) {
+        this.endPoint = endPoint;
+    }
+}
+}

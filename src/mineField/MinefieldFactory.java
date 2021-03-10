c93dbebb1d87b0f171b42263be11b310fe3cbc4c
Alan <alankash9@gmail.com>
2021-03-10 15:19:46 -0800

    Unfinished factory and appPanel files

--- src/mineField/MinefieldFactory.java
+++ src/mineField/MinefieldFactory.java
@@ -1,28 +1,45 @@
 package mineField;
 import mvc.*;
 
-/*
 public class MinefieldFactory implements AppFactory{
     public Model makeModel(){
-
+        return null;//finish
     }
     public View makeView(Model m){
-
+        return null;//finish
     }
     public String getTitle(){
-
+        return "Mine Field";
     }
     public String[] getHelp(){
-
+        //finish
+        return new String[]{"Click a button to move Rock.", "The number on each tile represents the number of mines surrounding it."};
     }
     public String about(){
-
+        return "Mine Field by Kevin Chu, Alan Kashiwagi, Jeremy Trieu";
     }
     public String[] getEditCommands(){
-
+        return new String[]{"N", "S", "W", "E", "NE","NW", "SE", "SW"};
     }
-    public Command makeEditCommand(Model model, String type){
+    public Command makeEditCommand(Model model, String type) {
+        Command cmmd = null;
+        if(type.equals("N")){
 
+        }else if(type.equals("S")){
+
+        }else if(type.equals("W")){
+
+        }else if(type.equals("E")){
+
+        }else if(type.equals("NE")){
+
+        }else if(type.equals("NW")){
+
+        }else if(type.equals("SE")){
+
+        }else if(type.equals("SW")){
+
+        }
+        return cmmd;
     }
 }
-*/
--- src/mineField/MinefieldPanel.java
+++ src/mineField/MinefieldPanel.java
@@ -1,8 +1,70 @@
 package mineField;
 import mvc.*;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MinefieldPanel extends AppPanel{
     public MinefieldPanel(AppFactory f) {
         super(f);
+        controlPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.CENTER;
+        c.weightx = 0.1;
+        c.weighty = 0.1;
+
+        JButton northButton = new JButton("N");
+        northButton.addActionListener(this);
+        c.gridx = 1;
+        c.gridy = 0;
+        controlPanel.add(northButton, c);
+
+        JButton southButton = new JButton("S");
+        c.gridx = 1;
+        c.gridy = 2;
+        southButton.addActionListener(this);
+        controlPanel.add(southButton, c);
+
+        JButton westButton = new JButton("W");
+        c.gridx = 0;
+        c.gridy = 1;
+        westButton.addActionListener(this);
+        controlPanel.add(westButton,c);
+
+        JButton eastButton = new JButton("E");
+        c.gridx = 2;
+        c.gridy = 1;
+        eastButton.addActionListener(this);
+        controlPanel.add(eastButton,c);
+
+        JButton northEastButton = new JButton("NE");
+        c.gridx = 2;
+        c.gridy = 0;
+        eastButton.addActionListener(this);
+        controlPanel.add(northEastButton,c);
+
+        JButton northWestButton = new JButton("NW");
+        c.gridx = 0;
+        c.gridy = 0;
+        eastButton.addActionListener(this);
+        controlPanel.add(northWestButton,c);
+
+        JButton southEastButton = new JButton("SE");
+        c.gridx = 2;
+        c.gridy = 2;
+        eastButton.addActionListener(this);
+        controlPanel.add(southEastButton,c);
+
+        JButton southWestButton = new JButton("SW");
+        c.gridx = 0;
+        c.gridy = 2;
+        eastButton.addActionListener(this);
+        controlPanel.add(southWestButton,c);
     }
+    public static void main(String[] args) {
+        AppFactory factory = new MinefieldFactory();
+        AppPanel panel = new MinefieldPanel(factory);
+        panel.display();
+    }
+
 }

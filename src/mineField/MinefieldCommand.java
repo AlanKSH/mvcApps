820d83c6e095716d9b43c86a732b80f58dab0d7b
KindredKinesis <kmhchu2001@gmail.com>
2021-03-10 15:00:45 -0800

    Create MinefieldCommand and MinefieldView files in mineField package

---
+++ src/mineField/MinefieldCommand.java
@@ -0,0 +1,14 @@
+package mineField;
+
+import mvc.*;
+
+public class MinefieldCommand extends Command {
+
+    public MinefieldCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+
+    }
+}
---
+++ src/mineField/MinefieldView.java
@@ -0,0 +1,9 @@
+package mineField;
+
+import mvc.*;
+import java.awt.*;
+
+public class MinefieldView extends View {
+
+    public MinefieldView(Model m) { super(m); }
+}

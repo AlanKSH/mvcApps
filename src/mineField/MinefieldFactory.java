df094a55eae8e233606b207febc858c483e8d374
Alan <alankash9@gmail.com>
2021-03-10 10:55:20 -0800

    0.3 Creating files for Mine Field

---
+++ src/mineField/MinefieldFactory.java
@@ -0,0 +1,28 @@
+package mineField;
+import mvc.*;
+
+/*
+public class MinefieldFactory implements AppFactory{
+    public Model makeModel(){
+
+    }
+    public View makeView(Model m){
+
+    }
+    public String getTitle(){
+
+    }
+    public String[] getHelp(){
+
+    }
+    public String about(){
+
+    }
+    public String[] getEditCommands(){
+
+    }
+    public Command makeEditCommand(Model model, String type){
+
+    }
+}
+*/
---
+++ src/mineField/MinefieldPanel.java
@@ -0,0 +1,8 @@
+package mineField;
+import mvc.*;
+
+public class MinefieldPanel extends AppPanel{
+    public MinefieldPanel(AppFactory f) {
+        super(f);
+    }
+}

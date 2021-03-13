bbd7b68e3d6a80d4e1d521b13f0ab7962610de04
KindredKinesis <kmhchu2001@gmail.com>
2021-03-12 16:23:11 -0800

    Update MinefieldCommand and MinefieldView, check if personal repository branch is formed

--- src/mineField/MinefieldCommand.java
+++ src/mineField/MinefieldCommand.java
@@ -3,12 +3,15 @@ package mineField;
 import mvc.*;
 
 public class MinefieldCommand extends Command {
+    Heading heading;
 
     public MinefieldCommand(Model model) {
         super(model);
     }
 
     public void execute() {
-
+        MineField minefield = (MineField) model;
+        //minefield.change();
+        minefield.move();
     }
 }
--- src/mineField/MinefieldView.java
+++ src/mineField/MinefieldView.java
@@ -5,5 +5,9 @@ import java.awt.*;
 
 public class MinefieldView extends View {
 
-    public MinefieldView(Model m) { super(m); }
+    public MinefieldView(MineField m) { super(m); }
+
+    public void paintComponent(Graphics gc) {
+        super.paintComponent(gc);
+    }
 }

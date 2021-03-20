2137b693e75a4a4da147dd0d9a53a1a3430705ac
KindredKinesis <kmhchu2001@gmail.com>
2021-03-20 15:03:45 -0700

    Change MinefieldView, now properly displays labels for undiscovered blocks

--- src/mineField/MinefieldView.java
+++ src/mineField/MinefieldView.java
@@ -9,21 +9,22 @@ import java.beans.PropertyChangeEvent;
 
 public class MinefieldView extends View {
     private Minefield mf;
-    private static int GRID_SIZE = 20;
-    private JLabel[][] labels;
+    private static Border BLOCK_UNSEEN = BorderFactory.createLineBorder(Color.BLACK);
+    private static Border BLOCK_VISITED = BorderFactory.createLineBorder(Color.WHITE);
+    private static int WORLD_SIZE = 20;
+    private JLabel[][] labels = new JLabel[WORLD_SIZE][WORLD_SIZE];;
 
     public MinefieldView(Model m) {
         super(m);
         mf = new Minefield();
-        labels = new JLabel[GRID_SIZE][GRID_SIZE];
 
         // Might need to use a constant for grid size instead, not sure
-        this.setLayout(new GridLayout(GRID_SIZE, GRID_SIZE));
+        this.setLayout(new GridLayout(WORLD_SIZE, WORLD_SIZE));
         // Display n x n JLabels containing the text ?
-
-        for(int i = 0; i < GRID_SIZE; i++) {
-            for(int j = 0; j < GRID_SIZE; j++) {
+        for(int i = 0; i < WORLD_SIZE; i++) {
+            for(int j = 0; j < WORLD_SIZE; j++) {
                 labels[i][j] = new JLabel("?");
+                labels[i][j].setBorder(BLOCK_UNSEEN);
                 this.add(labels[i][j]);
             }
         }
@@ -31,13 +32,12 @@ public class MinefieldView extends View {
 
     @Override
     public void propertyChange(PropertyChangeEvent arg0) {
-        super.propertyChange(arg0);
         // Locate the patch occupied by the player
         int currentX = mf.location.getXCoor();
         int currentY = mf.location.getYCoor();
+
         // Modify the label, border, and/or background color of the corresponding JLabel
-        labels[currentX][currentY] = new JLabel(""+mf.numsOfMine());
-        Border greenLine = BorderFactory.createLineBorder(Color.GREEN);
-        labels[currentX][currentY].setBorder(greenLine);
+        labels[currentX][currentY] = new JLabel(""+mf.getSurroundingMines());
+        labels[currentX][currentY].setBorder(BLOCK_VISITED);
     }
 }

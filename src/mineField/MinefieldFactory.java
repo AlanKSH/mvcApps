1cf51472ecd5134d374c6c33fad7a0e99a3e44c7
Alan <alankash9@gmail.com>
2021-03-20 18:21:18 -0700

    Changed help string

--- src/mineField/MinefieldFactory.java
+++ src/mineField/MinefieldFactory.java
@@ -13,7 +13,7 @@ public class MinefieldFactory implements AppFactory{
     }
     public String[] getHelp(){
         //finish
-        return new String[]{"Click a button to move Rock.", "The number on each tile represents the number of mines surrounding it."
+        return new String[]{"Click a button to move, but avoid moving onto mines.", "The number on each tile represents the number of mines surrounding it."
         , "Reach the bottom right tile to win."};
     }
     public String about(){

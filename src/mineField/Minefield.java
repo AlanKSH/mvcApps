4fabe3e5ee602e1f406c1cbe0806708b34a70312
Alan <alankash9@gmail.com>
2021-03-20 18:20:14 -0700

    move() throws exceptions to be caught by the appPanel in case the player wins or loses. Set the bottom right block to the winning block. Instead of "location Block" for the player location, it is determined by "int x,y"

--- src/mineField/Minefield.java
+++ src/mineField/Minefield.java
@@ -9,22 +9,21 @@ import mvc.*;
 
 public class Minefield extends Model {
     public static Integer WORLD_SIZE = 20;
-    int x,y;
+    int x, y;
     List<Block> path;
     boolean takenFlag;
     boolean checkMineFlag;
     private Heading direction;
-    Block [][] boardArray = new Block[WORLD_SIZE][WORLD_SIZE];
+    Block[][] boardArray = new Block[WORLD_SIZE][WORLD_SIZE];
 
     public Minefield() {
         x = y = 0;
-        for (int i = 0; i <WORLD_SIZE; i++)
-        {
-            for (int j = 0; j <WORLD_SIZE; j++)
-            {
-                boardArray[i][j]=new Block(i, j);
+        for (int i = 0; i < WORLD_SIZE; i++) {
+            for (int j = 0; j < WORLD_SIZE; j++) {
+                boardArray[i][j] = new Block(i, j);
             }
         }
+        boardArray[WORLD_SIZE-1][WORLD_SIZE-1].setEndPoint(true);
         path = new LinkedList<Block>();
         takenFlag = true;
         checkMineFlag = false;
@@ -33,7 +32,7 @@ public class Minefield extends Model {
     public List<Block> getPath() {
         return path;
     }
-    
+
     public void setTakenFlag() {
         takenFlag = true;
         path.add(boardArray[x][y]);
@@ -45,46 +44,47 @@ public class Minefield extends Model {
         Random rd = new Random();
         checkMineFlag = rd.nextBoolean();
     }
+
     private boolean isMineFlag() {
         if (checkMineFlag == true)
             return true;
         else return false;
     }
 
-    public int getYpos(){
+    public int getYpos() {
         return y;
     }
 
-    public int getXpos(){
+    public int getXpos() {
         return x;
     }
 
     public int getSurroundingMines() {
         int count = 0;
         List<Block> tempList = new LinkedList<Block>();
-        if(x!=0){
-            tempList.add(boardArray[x-1][y]);
+        if (x != 0) {
+            tempList.add(boardArray[x - 1][y]);
         }
-        if(y!=0){
-            tempList.add(boardArray[x][y-1]);
+        if (y != 0) {
+            tempList.add(boardArray[x][y - 1]);
         }
-        if(x!=WORLD_SIZE-1){
-            tempList.add(boardArray[x+1][y]);
+        if (x != WORLD_SIZE - 1) {
+            tempList.add(boardArray[x + 1][y]);
         }
-        if(y!=WORLD_SIZE-1){
-            tempList.add(boardArray[x][y+1]);
+        if (y != WORLD_SIZE - 1) {
+            tempList.add(boardArray[x][y + 1]);
         }
-        if(x!=0 &&y!=0){
-            tempList.add(boardArray[x-1][y-1]);
+        if (x != 0 && y != 0) {
+            tempList.add(boardArray[x - 1][y - 1]);
         }
-        if(x!= 0 && y!=WORLD_SIZE-1){
-            tempList.add(boardArray[x-1][y+1]);
+        if (x != 0 && y != WORLD_SIZE - 1) {
+            tempList.add(boardArray[x - 1][y + 1]);
         }
-        if(x!= WORLD_SIZE-1 && y!=0){
-            tempList.add(boardArray[x+1][y-1]);
+        if (x != WORLD_SIZE - 1 && y != 0) {
+            tempList.add(boardArray[x + 1][y - 1]);
         }
-        if(x!= WORLD_SIZE-1 && y!= WORLD_SIZE-1){
-            tempList.add(boardArray[x+1][y+1]);
+        if (x != WORLD_SIZE - 1 && y != WORLD_SIZE - 1) {
+            tempList.add(boardArray[x + 1][y + 1]);
         }
 
         for (Block x : tempList) {
@@ -93,135 +93,28 @@ public class Minefield extends Model {
         return count;
     }
 
-    /*public List<Block> getPredictedBlocks() {
-        List<Block> listBlock = new LinkedList<Block>();
-        List<Block> tempList = new LinkedList<Block>();
-        if ((x ==0)&& (y == 0)) {
-            Block b1 = new Block(x, y + 1);
-            Block b2 = new Block(x +1, y);
-            Block b3 = new Block(x + 1, y + 1);
-            tempList.add(b1);
-            tempList.add(b2);
-            tempList.add(b3);
-            
-        }
-        else if (x == 0) {
-            Block b1 = new Block(x, y - 1);
-            Block b2 = new Block(x +1, y - 1);
-            Block b3 = new Block(x + 1, y);
-            Block b4 = new Block(x + 1, y + 1);
-            Block b5 = new Block(x, y + 1);
-
-            tempList.add(b1);
-            tempList.add(b2);
-            tempList.add(b3);
-            tempList.add(b4);
-            tempList.add(b5);
-        }
-        else if (y == 0) {
-            Block b1 = new Block(x - 1, location.yCoor());
-            Block b2 = new Block(x - 1, location.yCoor() + 1);
-            Block b3 = new Block(x, location.yCoor() + 1);
-            Block b4 = new Block(x + 1, location.yCoor() + 1);
-            Block b5 = new Block(x + 1 , location.yCoor());
-
-            tempList.add(b1);
-            tempList.add(b2);
-            tempList.add(b3);
-            tempList.add(b4);
-            tempList.add(b5);
-        }
-        else if ((x == WORLD_SIZE - 1) && (y == 0)) {
-            Block b1 = new Block(x - 1, location.yCoor());
-            Block b2 = new Block(x - 1, location.yCoor() + 1);
-            Block b3 = new Block(x, location.yCoor() + 1);
-
-            tempList.add(b1);
-            tempList.add(b2);
-            tempList.add(b3);
-        }
-        else if (x == WORLD_SIZE - 1) {
-            Block b1 = new Block(x, location.yCoor() - 1);
-            Block b2 = new Block(x - 1, location.yCoor() - 1);
-            Block b3 = new Block(x - 1, location.yCoor());
-            Block b4 = new Block(x - 1, location.yCoor() + 1);
-            Block b5 = new Block(x, location.yCoor() + 1);
-
-            tempList.add(b1);
-            tempList.add(b2);
-            tempList.add(b3);
-            tempList.add(b4);
-            tempList.add(b5);
-        }
-        else if ((y == WORLD_SIZE - 1) && (x == 0)) {
-            Block b1 = new Block(x, location.yCoor() - 1);
-            Block b2 = new Block(x + 1, location.yCoor() - 1);
-            Block b3 = new Block(x + 1, location.yCoor());
-
-            tempList.add(b1);
-            tempList.add(b2);
-            tempList.add(b3);
-        }
-
-        else if (y == WORLD_SIZE - 1) {
-            Block b1 = new Block(x, location.yCoor() - 1);
-            Block b2 = new Block(x + 1, location.yCoor() - 1);
-            Block b3 = new Block(x + 1, location.yCoor());
-            Block b4 = new Block(x + 1, location.yCoor() + 1);
-            Block b5 = new Block(x, location.yCoor() + 1);
-
-            tempList.add(b1);
-            tempList.add(b2);
-            tempList.add(b3);
-            tempList.add(b4);
-            tempList.add(b5);
-        }
-
-        else {
-            Block b1 = new Block(x - 1, y);
-            Block b2 = new Block(x - 1, y + 1);
-            Block b3 = new Block(x, y + 1);
-            Block b4 = new Block(x + 1, y + 1);
-            Block b5 = new Block(x + 1, y);
-            Block b6 = new Block(x - 1, y - 1);
-            Block b7 = new Block(x, y - 1);
-            Block b8 = new Block(x + 1, y - 1);
-
-            tempList.add(b1);
-            tempList.add(b2);
-            tempList.add(b3);
-            tempList.add(b4);
-            tempList.add(b5);
-            tempList.add(b6);
-            tempList.add(b7);
-            tempList.add(b8);
-        }
-        for (Block x : tempList) {
-            if (path.contains(x)) continue;
-            else listBlock.add(x);
-        }
-
-        return listBlock;
-    }*/
 
     public boolean checkGetMined() {
-        if (path.get(path.size() - 1).blockHasMine() == true){
+        if (path.get(path.size() - 1).blockHasMine() == true) {
             return true;
             //System.out.println("Game Over!!!");
-        }
-        else return false;
+        } else return false;
 
     }
+
     public boolean checkGetHome() {
-        if ((x == WORLD_SIZE - 1) && 
-        (y == WORLD_SIZE - 1)) {
+        if ((x == WORLD_SIZE - 1) &&
+                (y == WORLD_SIZE - 1)) {
             return true;
             //win game!!!!
-        }
-        else return false;
+        } else return false;
 
     }
-    public void move() {
+
+    /* move method moves the player in the direction of the heading
+    throws exceptions if the player steps on a mine or wins
+     */
+    public void move() throws Exception {
         if (direction == Heading.NORTH) {
             y--;
         }
@@ -257,31 +150,26 @@ public class Minefield extends Model {
             x++;
             y++;
         }
-        if (x >= WORLD_SIZE ){
+        if (x >= WORLD_SIZE) {
             x--;
-        }
-        else if(x<0){
+        } else if (x < 0) {
             x++;
         }
-        if (y >= WORLD_SIZE ){
+        if (y >= WORLD_SIZE) {
             y--;
-        }
-        else if(y<0){
+        } else if (y < 0) {
             y++;
         }
         changed();
-    } 
+        if(boardArray[x][y].getEndPoint()){
+            throw new Exception("win");
+        }
+        if(boardArray[x][y].blockHasMine()){
+            throw new Exception("lose");
+        }
+    }
 
     public void turn(Heading direction) {
         this.direction = direction;
     }
-
-    /*public int numsOfMine() {
-        int count = 0;
-        List<Block> tempMineList = getPredictedBlocks();
-        for (int i = 0; i < tempMineList.size(); i++) {
-            if (tempMineList.get(i).blockHasMine() == true) count++;
-        }
-        return count;
-    }*/
 }

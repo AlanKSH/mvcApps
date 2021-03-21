178b0050a6e8f9eeb9b9e3cdbbee55095af49b08
Alan <alankash9@gmail.com>
2021-03-20 18:22:38 -0700

    setModel() overrides model.setModel() and resets all the labels.

--- src/mineField/MinefieldView.java
+++ src/mineField/MinefieldView.java
@@ -11,20 +11,35 @@ public class MinefieldView extends View {
     private Minefield mf;
     private static Border BLOCK_UNSEEN = BorderFactory.createLineBorder(Color.BLACK);
     private static Border BLOCK_VISITED = BorderFactory.createLineBorder(Color.WHITE);
-    private static int WORLD_SIZE = 20;
-    private JLabel[][] labels = new JLabel[WORLD_SIZE][WORLD_SIZE];;
+    private JLabel[][] labels;
 
     public MinefieldView(Model m) {
         super(m);
         mf = (Minefield) model;
         // Might need to use a constant for grid size instead, not sure
-        this.setLayout(new GridLayout(WORLD_SIZE, WORLD_SIZE));
-        // Display n x n JLabels containing the text ?
-        for(int j = 0; j < WORLD_SIZE; j++) {
-            for(int i = 0; i < WORLD_SIZE; i++) {
+        labels = new JLabel[Minefield.WORLD_SIZE][Minefield.WORLD_SIZE];
+        this.setLayout(new GridLayout(Minefield.WORLD_SIZE, Minefield.WORLD_SIZE));
+        // j indicates column, i indicates row
+        for(int j = 0; j < Minefield.WORLD_SIZE; j++) {
+            for(int i = 0; i < Minefield.WORLD_SIZE; i++) {
                 labels[i][j] = new JLabel("?");
                 labels[i][j].setBorder(BLOCK_UNSEEN);
                 this.add(labels[i][j]);
+            }
+        }
+        model.changed();
+    }
+
+    /* setModel method call model.setModel and redraws all the labels
+    Currently does not draw visited tiles when opening a model
+     */
+    public void setModel(Model m){
+        super.setModel(m);
+        mf = (Minefield) m;
+        for(int j = 0; j < Minefield.WORLD_SIZE; j++) {
+            for(int i = 0; i < Minefield.WORLD_SIZE; i++) {
+                labels[i][j].setText("?");
+                labels[i][j].setBorder(BLOCK_UNSEEN);
             }
         }
         model.changed();
@@ -39,6 +54,5 @@ public class MinefieldView extends View {
         // Modify the label, border, and/or background color of the corresponding JLabel
         labels[currentX][currentY].setText(String.valueOf(mf.getSurroundingMines()));
         labels[currentX][currentY].setBorder(BLOCK_VISITED);
-        repaint();
     }
 }
